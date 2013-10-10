606dfee274dbace35874f44cc31d947caf5a6323
nycz <pyntix@gmail.com>
2013-10-10 14:21:32 +0200

    Added option to show if the file is being logged
    Fixes #1

--- kalpana-logger.py
+++ kalpana-logger.py
@@ -17,7 +17,7 @@ class UserPlugin(GUIPlugin):
         self.textarea.file_saved.connect(self.on_save)
         self.configpath = objects['settings manager'].get_config_directory()
         self.local_path = get_path()
-        self.commands = {'l': (self.start_logging, 'Start logging this file (y for offset, n for no offset)')}
+        self.commands = {'l': (self.start_logging, 'Start logging this file (y for offset, n for no offset, omit argument for only info)')}
 
     def read_config(self):
         configfile = join(self.configpath, 'kalpana-logger.conf')
@@ -31,33 +31,40 @@ class UserPlugin(GUIPlugin):
         if not filepath:
             self.error('Can\'t log an unnamed file! Save and try again (without offset)')
             return
+        # Paths
+        logdir, indexpath, relative_filepath = self.get_logpaths()
+        index = get_index(indexpath)
+        if not arg:
+            if relative_filepath in index:
+                self.print_('File is being logged.')
+            else:
+                self.print_('File is not being logged.')
+            return
         if arg not in ('y','n'):
             self.error('Argument should be y/n! (use offset/no offset)')
             return
-        # Offset
+        if relative_filepath in index:
+            self.error('File already logged!')
+            return
+        # Offsets
         if arg == 'y':
             offset = len(re.findall(r'\S+', read_file(self.textarea.file_path)))
         else:
             offset = 0
-        # Paths
-        logdir, indexpath, rel_fpath = self.get_logpaths()
-        index = get_index(indexpath)
-        if rel_fpath in index:
-            self.error('File already logged!')
-            return
         # Init
-        logfname = generate_logfilename(logdir, rel_fpath)
-        index[rel_fpath] = logfname
+        logfname = generate_logfilename(logdir, relative_filepath)
+        index[relative_filepath] = logfname
         write_file(join(logdir, logfname), str(offset) + '\n')
         write_json(indexpath, index)
         self.print_('Started logging file!')
 
     def on_save(self):
-        logdir, indexpath, rel_fpath = self.get_logpaths()
+        logdir, indexpath, relative_filepath = self.get_logpaths()
         index = get_index(indexpath)
-        if rel_fpath not in index:
+        # Abort if the current file isn't in the index (= isn't being logged)
+        if relative_filepath not in index:
             return
-        logfile = join(logdir, index[rel_fpath])
+        logfile = join(logdir, index[relative_filepath])
         wc = self.textarea.get_wordcount()
         lastwc = read_file(logfile).splitlines()[-1].split(';',1)[-1]
         if int(lastwc) == wc:
@@ -67,13 +74,20 @@ class UserPlugin(GUIPlugin):
             f.write(data)
 
     def get_logpaths(self):
+        """
+        Return the different relevant paths.
+
+        logdir             Absolute path to the directory containing the logfiles
+        indexpath          Absolute path to the indexfile for all logfiles
+        relative_filepath  Relative path to the currently open file in kalpana
+        """
         rootdir = fixpath(self.settings['rootdir'])
         logdir = join(rootdir, self.settings['logdir'])
         if not exists(logdir):
             os.makedirs(logdir, mode=0o755, exist_ok=True)
         indexpath = join(logdir, 'index.json')
-        rel_fpath = fixpath(self.textarea.file_path).lstrip(rootdir).lstrip(sep)
-        return logdir, indexpath, rel_fpath
+        relative_filepath = fixpath(self.textarea.file_path).lstrip(rootdir).lstrip(sep)
+        return logdir, indexpath, relative_filepath
 
 
 def get_index(path):
